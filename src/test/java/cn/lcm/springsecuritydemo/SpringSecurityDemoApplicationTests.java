421aa47a97bdc3c5a9a5bad67129dbc91c839727
liaocm <122789454@qq.com>
2020-04-13 15:01:07 +0800

    first

---
+++ src/test/java/cn/lcm/springsecuritydemo/SpringSecurityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.lcm.springsecuritydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
